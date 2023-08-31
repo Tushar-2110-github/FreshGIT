f115add80874d738ef917043ae351a67b4975176
Tushar <tushar.tusharbramhankar21@gmail.com>
2023-08-31 22:10:19 +0530

    Adding table of 5

---
+++ GitDEMO/src/com/git/demo/TableOf5.java
@@ -0,0 +1,10 @@
+package com.git.demo;
+
+public class TableOf5 {
+	public static void main(String[] args) {
+		int s =5;
+		for (int i = 1; i <= 10; i++) {
+System.out.println(s + "*" + i + "=" + (s*i));
+		}
+	}
+}
